0eabedab38b0446a385c02b6d16545f89650f54b
Zaronology <dave@zaronology.com>
2025-11-10 15:07:23 -0500

    updated

--- src/app/(pages)/blog/_components/blog/component-map.ts
+++
@@ -1,6 +0,0 @@
-import dynamic from "next/dynamic";
-
-export const componentMap = {
-  "blog.text-block": dynamic(() => import("./text-block"), { ssr: true }),
-  "blog.media": dynamic(() => import("./media"), { ssr: true }),
-};
--- src/app/(pages)/blog/_components/ui/TopicCarousel/index.tsx
+++
@@ -1,3 +0,0 @@
-export default function TopicCarousel() {
-  return <div></div>;
-}
